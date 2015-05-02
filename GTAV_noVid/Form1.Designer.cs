a257277583355618281c249883e408761756acef
xeramon <xeramon@live.de>
2015-05-02 21:35:45 +0200

    Initial upload

---
+++ GTAV_noVid/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace GTAV_noVid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPatch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDebug = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPatch
+            // 
+            this.btnPatch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPatch.Location = new System.Drawing.Point(497, 12);
+            this.btnPatch.Name = "btnPatch";
+            this.btnPatch.Size = new System.Drawing.Size(99, 23);
+            this.btnPatch.TabIndex = 0;
+            this.btnPatch.Text = "Patch GTA5.exe";
+            this.btnPatch.UseVisualStyleBackColor = true;
+            this.btnPatch.Click += new System.EventHandler(this.btnPatch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "This tool cuts the into video from GTA V. \r\nYou will need to use this tool after " +
+    "every patch.  \r\nWarning: This could be considered as a cheat since it edits the " +
+    "GTA5.exe";
+            // 
+            // lblDebug
+            // 
+            this.lblDebug.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDebug.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lblDebug.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDebug.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblDebug.Location = new System.Drawing.Point(103, 50);
+            this.lblDebug.Multiline = true;
+            this.lblDebug.Name = "lblDebug";
+            this.lblDebug.ReadOnly = true;
+            this.lblDebug.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.lblDebug.Size = new System.Drawing.Size(496, 119);
+            this.lblDebug.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::GTAV_noVid.Properties.Resources._4;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 175);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 173);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblDebug);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPatch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "༼ つ ◕_◕ ༽つ GTAV - No intro video - by Xeramon v0.9.8";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPatch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lblDebug;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GTAV_noVid/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace GTAV_noVid
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Disclaimer for people who decompled it: Yes, I am not the best coder.
+        //What it does: It just searches for 706C6174666F726D3A2F6D6F766965732F726F636B737461725F6C6F676F730062696B
+        //in the GTA5.exe and overwrites it with zeros.
+        //Please give me credit if you want to modify the tool.
+        //test123
+
+        string filePath = "C:/";
+        string stringDebug = "";
+        int firstByte = -1, lastByte = -1, lenghtByte;
+        string installpath = "C:/";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            if (r.Next(1, 4) == 1)
+            {
+                pictureBox1.Image = global::GTAV_noVid.Properties.Resources._1;
+            }
+            else
+            {
+                if (r.Next(1, 4) == 2)
+                {
+                    pictureBox1.Image = global::GTAV_noVid.Properties.Resources._2;
+                }
+                else
+                {
+                    if (r.Next(1, 4) == 3)
+                    {
+                        pictureBox1.Image = global::GTAV_noVid.Properties.Resources._3;
+                    }
+                    else
+                        pictureBox1.Image = global::GTAV_noVid.Properties.Resources._4;
+                }
+            }
+
+        }
+
+
+        private void btnPatch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RegistryKey regKey = Registry.LocalMachine;
+                regKey = regKey.OpenSubKey(@"Software\Wow6432Node\rockstar games\GTAV");
+
+                if (regKey != null)
+                {
+                    installpath = regKey.GetValue("InstallFolderSteam").ToString();
+                    installpath = installpath.Remove(installpath.Length - 4);
+                }
+
+                else
+                {
+                    RegistryKey regKey2 = Registry.LocalMachine;
+                    regKey = regKey.OpenSubKey(@"Software\Wow6432Node\rockstar games\Grand Theft Auto V");
+
+                    if (regKey != null)
+                    {
+                        installpath = regKey.GetValue("InstallFolder").ToString();
+                    }
+                
+                }
+            }
+            catch {}
+            //HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\rockstar games
+            OpenFileDialog fileOpenDialog = new OpenFileDialog();
+            fileOpenDialog.Title = "Open GTA5.exe";
+            fileOpenDialog.InitialDirectory = Path.GetFullPath(installpath);
+            fileOpenDialog.RestoreDirectory = true;
+            fileOpenDialog.Filter = "GTA5.exe|GTA5.exe|All files (*.*)|*.*";
+            fileOpenDialog.FilterIndex = 1;
+
+            lblDebug.Text += "installpath: " + installpath + "\r\n";
+
+            if (fileOpenDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                filePath = fileOpenDialog.FileName;
+                //filePath = lblPath.Text;
+                lblDebug.Text += "filePath: " + filePath + "\r\n";
+
+            }
+
+            byte[] pattern = { 112, 108, 97, 116, 102, 111, 114, 109, 58, 47, 109, 111, 118, 105, 101, 115, 47, 114, 111, 99, 107, 115, 116, 97, 114, 95, 108, 111, 103, 111, 115, 0, 98, 105, 107 };
+            byte[] data = {0};
+
+            try
+            {
+                data = File.ReadAllBytes(@filePath);
+            }
+            catch { }
+
+            for (int i = 0; i < data.Length - pattern.Length; i++)
+            {
+                bool match = true;
+                for (int j = 0; j < pattern.Length; j++)
+                {
+                    if (data[i + j] != pattern[j])
+                    {
+                        match = false;                     
+                        break;
+                    }
+                }
+
+                if (match)
+                {
+                    //stringDebug = (i + pattern.Length) + "";
+                    firstByte = i;
+                    lastByte = i + pattern.Length;
+                    lenghtByte = pattern.Length;
+                    stringDebug += "Intro video found! \r\n";
+                    stringDebug += "First byte: " + firstByte.ToString("X") + " \r\nLast byte: " + lastByte.ToString("X") + "\r\n";
+                    stringDebug += "Debug: "+ getBytesDec(firstByte, lenghtByte) + "\r\n";
+                    stringDebug += "Intro video was successful cut." + "\r\n";
+                    break;
+
+                }
+                else
+                {
+                    stringDebug = "No intro video found." + "\r\n" ;
+
+                }
+            }
+            lblDebug.Text += stringDebug + "\r\n";
+        }
+
+        private int getBytesDec(int readStart, int lenghtByte)
+        {
+            byte[] bClean = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            
+
+            var f = new FileStream(@filePath, FileMode.Open, FileAccess.Write);
+            f.Seek(readStart, SeekOrigin.Begin);
+            f.Write(bClean, 0, bClean.Length);
+            f.Close();
+
+            return 0;
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//made by xeramon from reddit.com/u/xeramon
